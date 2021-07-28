83dc34f3bc45cc0eef613264314c0fe75a2beb6d
Ahmed Mohamed <ahmed180038@fsc.bu.edu.eg>
2021-07-28 03:36:04 +0200

    .

---
+++ lib/models/cubit/cubit.dart
@@ -0,0 +1,9 @@
+// import 'package:flutter/material.dart';
+// import 'package:bloc/bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:quran_radio/models/states/states.dart';
+//
+// class Appcubit extends Cubit<AppState> {
+//   Appcubit() : super(AppintiState());
+//   static Appcubit get(context) => BlocProvider.of(context);
+// }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,57 @@
 import 'package:flutter/material.dart';
 
+import 'package:assets_audio_player/assets_audio_player.dart';
+
 class HomeScreen extends StatelessWidget {
+  AssetsAudioPlayer audioStreamPlayer = AssetsAudioPlayer();
+  Future playaudio() async {
+    try {
+      await audioStreamPlayer.open(
+        Audio.liveStream('http://live.mp3quran.net:9718/'),
+        showNotification: true,
+      );
+    } catch (t) {
+      print(t);
+    }
+  }
+
+  pauseaudio() {
+    audioStreamPlayer.pause();
+  }
+
+  stopaudio() {
+    audioStreamPlayer.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          MaterialButton(
+            onPressed: () {
+              playaudio();
+            },
+            child: Text('play'),
+            color: Colors.red,
+          ),
+          MaterialButton(
+            onPressed: () {
+              pauseaudio();
+            },
+            child: Text('pause'),
+            color: Colors.red,
+          ),
+          MaterialButton(
+            onPressed: () {
+              stopaudio();
+            },
+            child: Text('stop'),
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
   }
 }
